78ed2fd68399d5600c3959219c987bc561393487
Saini <AR8I38C@apac.vwg>
2021-10-02 18:42:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React , {useState,useEffect} from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import TodosList from "./components/TodosList";
+import './App.css';
+
+const App = () => {
+
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(initialState);
+  const [editTodo, setEditTodo] = useState(null);
+  
+  useEffect(() => {
+    localStorage.setItem("todos",JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+        <Header />
+      </div>
+      <div>
+        <Form 
+        input={input}
+        setInput={setInput}
+        todos={todos}
+        setTodos={setTodos}
+        editTodo={editTodo}
+        setEditTodo={setEditTodo}
+        />
+      </div>
+      <div>
+        <TodosList 
+        todos={todos} 
+        setTodos={setTodos}
+        setEditTodo={setEditTodo}/>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Task Reminder</h1>
+        </div>
+    );
+};
+
+export default Header;
